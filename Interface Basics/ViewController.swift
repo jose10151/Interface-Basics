199824b76d937f9a5358c8f089bd8f4971a98fbc
Jose Trujillo <joseh.trujillo11@gmail.com>
2023-09-19 13:59:02 -0400

    yes

--- Interface Basics/ViewController.swift
+++ Interface Basics/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var TextField: UITextField!
+    @IBOutlet weak var secondlabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func Button(_ sender: Any) {
+        secondlabel.text = TextField.text
+    }
+    
 }
+
 
